2fcda17a9ee4c8ee7430fcf9e6423deff9fb2fb3
MaksMusic <Maksmusic05@yandex.ru>
2023-09-28 13:09:36 +0300

    work with ArrayBlockingQueue

---
+++ src/main/java/SynchronizedCollections/ArrayBlockingQueueRunnableEx.java
@@ -0,0 +1,43 @@
+package SynchronizedCollections;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ArrayBlockingQueueRunnableEx  {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Integer> arrayBlockingQueue =
+                new ArrayBlockingQueue<>(5);
+
+
+        //Producer
+        new Thread(()->{
+            int i = 0;
+            while (true){
+                try {
+                    arrayBlockingQueue.put(++i);
+                    System.out.println("Producer add number " + i);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+        //Consumer
+        new Thread(()->{
+            while (true){
+                Integer j = null;
+                try {
+                    j = arrayBlockingQueue.take();
+                    System.out.println("Consumer zabral " +j);
+                    Thread.sleep(9000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+    }
+}
+
+
